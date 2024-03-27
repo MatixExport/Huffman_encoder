6cda42aa60ee420fcd88e286f7d165cca036e486
Mateusz Giełczynski <mateusz.gielczynski@outlook.com>
2024-03-27 21:41:48 +0100

    bugfix: revert of "proper recursion" and more versatile tre generator

--- TreeGenerator.py
+++ TreeGenerator.py
@@ -2,33 +2,36 @@ from TreeNode import TreeNode
 
 
 class TreeGenerator:
-    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'w',
-             'x', 'y', 'z', ' ']
-    char_count = [0 for i in chars]
+    char_dict = {}
 
     def __init__(self):
-        self.nodes = []
-        self.text = "ala ma kota"
+        self.node = None
+        self.text = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
 
     def count_chars(self):
-        for i in range(len(self.chars)):
-            self.char_count[i] = self.text.count(self.chars[i])
+        for char in self.text:
+            if char in self.char_dict.keys():
+                self.char_dict[char] += 1
+            else:
+                self.char_dict[char] = 1
 
     def generate_tree(self):
-        for char, count in zip(self.chars, self.char_count):
-            if count > 0:
-                self.nodes.append(TreeNode(char, count))
+        nodes = []
+        for char in self.char_dict:
+            nodes.append(TreeNode(char, self.char_dict[char]))
+        for node in nodes:
+            print(node, node.char)
+        while len(nodes) > 1:
+            nodes.sort(key=lambda x: x.value)
+            nodes.append(TreeNode('', nodes[0].value + nodes[1].value, nodes[0], nodes[1]))
+            nodes.remove(nodes[0])
+            nodes.remove(nodes[0])
 
-        while len(self.nodes) > 1:
-
-            self.nodes.sort(key=lambda x: x.value)
-            self.nodes.append(TreeNode('', self.nodes[0].value + self.nodes[1].value, self.nodes[0], self.nodes[1]))
-            self.nodes.remove(self.nodes[0])
-            self.nodes.remove(self.nodes[0])
+        self.node = nodes[0]
 
     def get_navigation_path(self, char):
         path = ""
-        node = self.nodes[0]
+        node = self.node
         while node.char != char:
             if node.left.has_char(char):
                 path += "0"
@@ -42,6 +45,15 @@ class TreeGenerator:
 
 tr = TreeGenerator()
 tr.count_chars()
+
 tr.generate_tree()
+
+encoded_chars = {}
+
 for char in tr.text:
-    print(tr.get_navigation_path(char))
+    if char not in encoded_chars.keys():
+        encoded_chars[char] = tr.get_navigation_path(char)
+
+values = list(encoded_chars.values())
+values.sort()
+print(values)
