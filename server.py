47a93b204056e155ad19db4cde175d6caaac3080
247662 Giełczyński Mateusz <247662@edu.p.lodz.pl>
2024-04-19 00:46:09 +0200

    add: comments explaining some random numbers

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ sock.bind(('192.168.1.17',8088))    #to je moje ip w domu
 sock.listen(100)
 while True:
     client,addr = sock.accept()
-    codebook = client.recv(100)
+    codebook = client.recv(26)    #count of chars in codebook
     lengths = []
     for byte in codebook:
         lengths.append(int(byte))
     codebook = generate_codebook(lengths)
-    message = client.recv(100)
+    message = client.recv(100)  #message len
 
     print(decode(message, codebook))
     break
